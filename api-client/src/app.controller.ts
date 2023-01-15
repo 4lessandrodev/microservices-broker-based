893b2afba7254e75d542a8881934e36d68eb7eb5
4lessandrodev <alessandroadm@live.com>
2023-01-15 15:34:57 -0300

    feat(conn): ensure wait until connect

--- api-client/src/app.controller.ts
+++ api-client/src/app.controller.ts
@@ -13,15 +13,15 @@ export class AppController {
   constructor(rabbitMQ: RabbitMqFactory) {
     this.brokerPayment = rabbitMQ.paymentQueue()
     this.brokerInvoices = rabbitMQ.invoicesQueue();
-    this.brokerInvoices.connect();
-    this.brokerPayment.connect();
+    this.brokerInvoices.connect().catch(console.log);
+    this.brokerPayment.connect().catch(console.log);
   }
 
   @Post('/buy')
   @UsePipes(ValidationPipe)
   async buy(@Body() dto: CreateDataDto) {
-    this.brokerInvoices.connect();
-    this.brokerPayment.connect();
+    await this.brokerInvoices.connect();
+    await this.brokerPayment.connect();
     await firstValueFrom(this.brokerPayment.emit('@payment', dto));
     await firstValueFrom(this.brokerInvoices.emit('@invoice', dto));
     this.brokerInvoices.close();
@@ -31,17 +31,17 @@ export class AppController {
 
   @Get('/payments')
   async getPayments(): Promise<Array<CreateDataDto>> {
-    this.brokerPayment.connect();
+    await this.brokerPayment.connect();
     const result = await firstValueFrom<Array<CreateDataDto>>(this.brokerPayment.send('@get-payments', {}));
-    this.brokerPayment.close();
+    await this.brokerPayment.close();
     return result;
   }
 
   @Get('/invoices')
   async getInvoices(): Promise<Array<CreateDataDto>> {
-    this.brokerInvoices.connect();
+    await this.brokerInvoices.connect();
     const result = await firstValueFrom<Array<CreateDataDto>>(this.brokerInvoices.send('@get-invoices', {}));
-    this.brokerInvoices.close();
+    await this.brokerInvoices.close();
     return result;
   }
 }
